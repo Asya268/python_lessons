73b8bfb722cfec0e0d3cbb173bd2c2d04d8f9859
Asya268 <asya.zhila@gmail.com>
2025-11-23 16:56:45 +0400

    create branch

---
+++ lesson3/smartphone.py
@@ -0,0 +1,14 @@
+class Smartphone:
+    def __init__(self, brand_phone, model_phone, number_phone):
+        self.brand_phone = brand_phone
+        self.model_phone = model_phone
+        self.number_phone = number_phone
+
+    def get_brand_phone(self):
+        return self.brand_phone
+
+    def get_model_phone(self):
+        return self.model_phone
+
+    def get_number_phone(self):
+        return self.number_phone
