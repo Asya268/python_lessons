463d046240291b3236d75b9e9745d688c68e9380
Asya268 <asya.zhila@gmail.com>
2026-01-16 14:25:50 +0400

    Удалена папка 07_lesson

--- 07_lesson/pages/cart.py
+++
@@ -1,9 +0,0 @@
-from pages.base import BasePage
-from selenium.webdriver.common.by import By
-
-
-class CartPage(BasePage):
-    CHECKOUT_BUTTON = (By.ID, "checkout")
-
-    def click_checkout(self):
-        self.find_element(self.CHECKOUT_BUTTON).click()
